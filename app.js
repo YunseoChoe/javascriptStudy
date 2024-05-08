952b77d7d180cfd86940adf4f5dad235d6729a54
choeyunseo <choeyunseo4060@naver.com>
2024-05-09 00:20:17 +0900

    Update app.js, index.html, sytle.css

--- app.js
+++ app.js
@@ -67,6 +67,9 @@
 // console.log(hellos.length);
 
 const title = document.querySelector(".hello h1"); // page에서 element 찾아오기
+// Step 1. find the element
+const h1 = document.querySelector("div.hello:first-child h1");
+
 // const title = document.querySelectorAll(".hello h1");
 
 // const title1 = document.querySelector("#hello");
@@ -78,12 +81,20 @@ const title = document.querySelector(".hello h1"); // page에서 element 찾아�
 // console.log(title1);
 // console.log(title2);
 
-console.dir(title);
-title.style.color = "blue";
+// console.dir(title);
+// title.style.color = "blue";
 
+// Step 3. react to the event
 function handleTitleClick() {
-    console.log("title was clicked");
-    title.style.color = "red";
+    const currentColor = h1.style.color;
+    let newColor; // 변경될 수 있음
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    }   
+    else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
 function mouseenter() {
@@ -111,12 +122,30 @@ function handleWindowOnline() {
     alert("ALL GOOOD");
 }
 
-title.addEventListener("click", handleTitleClick); // event listen
-// title.onclick = handleTitleClick;
-title.addEventListener("mouseenter", mouseenter); 
-title.addEventListener("mouseleave", mouseleave);
+// title.addEventListener("click", handleTitleClick); // event listen
+// // title.onclick = handleTitleClick;
+// title.addEventListener("mouseenter", mouseenter); 
+// title.addEventListener("mouseleave", mouseleave);
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+// window.addEventListener("resize", handleWindowResize);
+// window.addEventListener("copy", handleWindowCopy);
+// window.addEventListener("offline", handleWindowOffline);
+// window.addEventListener("online", handleWindowOnline);
+
+// Step 2. listen the event
+h1.addEventListener("click", handleTitleClick);
+
+const h_1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+    // const clickedClass = "clicked";
+    // if (h_1.classList.contains(clickedClass)) {
+    //     h_1.classList.remove(clickedClass);
+    // }
+    // else {
+    //     h_1.classList.add(clickedClass);
+    // }
+    h_1.classList.toggle("clicked");
+}
+
+h_1.addEventListener("click", handleTitleClick);
