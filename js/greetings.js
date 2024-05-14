787939e1fa158b9fd8117c363a9b7154e3d5d357
choeyunseo <choeyunseo4060@naver.com>
2024-05-14 23:23:27 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -6,6 +6,7 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username"; // string들은 대문자 변수로 저장해놓는 것이 JS에서는 오류 검출에 좋음
 
+
 function onLoginSubmit(event) {
     event.preventDefault(); // 폼을 눌렀을 때 페이지를 새로고침하는 기본 동작을 막음
     loginForm.classList.add(HIDDEN_CLASSNAME); 
@@ -29,9 +30,10 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
     // show the form
     loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener
+    loginForm.addEventListener("submit", onLoginSubmit);
 }
 else {
     // show the greetings
     paintGreetings(savedUsername);
 }
+
